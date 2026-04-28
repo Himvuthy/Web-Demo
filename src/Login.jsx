5ddc136aed1583f51e10a01810b71275f0f2cf2c
Himvuthy <103051264+Himvuthy@users.noreply.github.com>
2026-04-28 11:15:32 +0700

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -36,7 +36,7 @@ const Login = ({ onLoginSuccess }) => {
     setIsLoading(true); 
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/login', {
+      const response = await fetch('https://web-demo-production-7fc6.up.railway.app/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
